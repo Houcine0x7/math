832409a32157df01e6555ff97ddf567194b334d1
Houcine0x7 <121579234+Houcine0x7@users.noreply.github.com>
2023-01-17 17:21:16 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    if (4 / 2 == 2) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (6 / 2 == 1) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
